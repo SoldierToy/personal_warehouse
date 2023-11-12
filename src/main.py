9bfa58adbc88af26804600f839d3b2cbd35d8248
soldiertoy <soldiertoy01@gmail.com>
2023-11-12 22:43:48 +0700

    del metadata.create_all

--- src/main.py
+++ src/main.py
@@ -1,21 +1,8 @@
-from contextlib import asynccontextmanager
 import uvicorn
 from fastapi import FastAPI
 from src.api_v1 import router as v1_router
-from src.db.db import engine
-from src.models.base import BaseORM
 
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    print(BaseORM.metadata.info)
-    async with engine.begin() as conn:
-        await conn.run_sync(BaseORM.metadata.create_all)
-
-    yield
-
-
-app = FastAPI(title="Personal warehouse", lifespan=lifespan)
+app = FastAPI(title="Personal warehouse")
 
 app.include_router(router=v1_router, tags=['auth'], prefix='/api/v1')
 
