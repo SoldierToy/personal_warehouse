6eedfe7d11504dfd6c8d80a484f265cde13b8cea
soldiertoy <soldiertoy01@gmail.com>
2023-11-18 01:13:41 +0700

    change port

--- src/main.py
+++ src/main.py
@@ -7,4 +7,4 @@ app = FastAPI(title="Personal warehouse")
 app.include_router(router=v1_router, prefix='/api/v1')
 
 if __name__ == '__main__':
-    uvicorn.run("src.main:app", reload=True)
+    uvicorn.run("src.main:app", port=8000, reload=True)
