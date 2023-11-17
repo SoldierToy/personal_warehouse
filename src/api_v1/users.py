bf67d651f81ef904870db6ce463ee6de765fc081
soldiertoy <soldiertoy01@gmail.com>
2023-11-18 01:13:23 +0700

    create endpoint "create_users"

--- src/api_v1/users.py
+++ src/api_v1/users.py
@@ -1,4 +1,10 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+from src.schemas.users import UsersSchema
+from src.services.users_service import UsersService
+
+from src.dependencies.depends_users_service import users_service
 
 router = APIRouter(prefix="/users", tags=['users'])
 
@@ -6,3 +12,14 @@ router = APIRouter(prefix="/users", tags=['users'])
 @router.get('/')
 async def get_users():
     print("get_users")
+
+
+@router.post('/')
+async def create_user(
+        user: UsersSchema,
+        user_service: Annotated[UsersService, Depends(users_service)]
+):
+    print(user)
+    print(user_service)
+    user_id = await user_service.add_user(user)
+    print(user_id)
