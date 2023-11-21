070c1f4dd430b03b3e2e59889f701e2db19df3e7
soldiertoy <soldiertoy01@gmail.com>
2023-11-21 22:49:09 +0700

    fix models

--- src/models/base.py
+++ src/models/base.py
@@ -4,5 +4,5 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 class BaseORM(DeclarativeBase):
     __abstract__ = True
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str]
