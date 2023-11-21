0ec789410eaa0435c6d2dd86cf677c4b38c08b31
soldiertoy <soldiertoy01@gmail.com>
2023-11-21 22:50:34 +0700

    create endpoints

--- src/schemas/products.py
+++ src/schemas/products.py
@@ -1,21 +1,40 @@
 import datetime
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
-class ProductsSchema(BaseModel):
-    id: int
+class ProductsCreateSchema(BaseModel):
     name: str
-    user_id: int
-    product_type: str
-    price: Optional[int]
-    selling_price: Optional[int]
-    repair_price:Optional[int]
-    profit: Optional[int]
-    receipt_date: Optional[datetime.date]
-    sale_date: Optional[datetime.date]
-    comment: Optional[str]
-    place: Optional[str]
-    status: Optional[str]
-    product_location: Optional[str]
+    user_fk: int
+    product_type_fk: int | None = None
+    price: int | None = None
+    selling_price: int | None = None
+    repair_price: int | None = None
+    profit: int | None = None
+    receipt_date: Optional[datetime.date] = None
+    sale_date: Optional[datetime.date] = None
+    comment: Optional[str] = None
+    place_fk: int | None = None
+    status_fk: int | None = None
+    product_location_fk: int | None = None
+
+
+class ProductDeleteSchema(BaseModel):
+    id: int
+
+
+class ProductSchema(BaseModel):
+    id: int
+
+
+class ProductsReadOneSchema(ProductsCreateSchema):
+    id: int
+
+
+class ProductUpdate(BaseModel):
+    id: int
+
+
+class ProductCreateResponse(BaseModel):
+    id: int
